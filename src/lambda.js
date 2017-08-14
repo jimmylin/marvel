30b71b80fb48bba979cf85ad2be0028552f15f3b
Jimmy Lin <jimmylin@Jimmys-MacBook.fios-router.home>
2017-08-13 23:26:05 -0400

    Updated to new code using the alexa-sdk

--- src/lambda.js
+++ src/lambda.js
@@ -1,221 +1,141 @@
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
- * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
- * testing instructions are located at http://amzn.to/1LzFrj6
- *
- * For additional samples, visit the Alexa Skills Kit Getting Started guide at
- * http://amzn.to/1LGWsLG
- */
+// alexa-cookbook sample code
 
-var http = require('http'); //Used to make an http call to the marvel api
-var crypto = require('crypto');  //Marvel requires you send them a hash of the api key and timestamp
+// There are three sections, Text Strings, Skill Code, and Helper Function(s).
+// You can copy and paste the entire file contents as the code for a new Lambda function,
+//  or copy & paste section #3, the helper function, to the bottom of your existing Lambda code.
 
-// Route the incoming request based on type (LaunchRequest, IntentRequest,
-// etc.) The JSON body of the request is provided in the event parameter.
-exports.handler = function (event, context) {
-    try {
-        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
 
-        /**
-         * Uncomment this if statement and populate with your skill's application ID to
-         * prevent someone else from configuring a skill that sends requests to this function.
-         */
-        /*
-        if (event.session.application.applicationId !== "") {
-             context.fail("Invalid Application ID");
+// 1. Text strings =====================================================================================================
+//    Modify these strings and messages to change the behavior of your Lambda function
+
+//var myRequest = 'batman';
+
+// 2. Skill Code =======================================================================================================
+
+
+var Alexa = require('alexa-sdk');
+
+var comicvine_apikey = '<<ENTER YOUR API KEY HERE>>';
+
+// TODO Change the callback function to pass the images back from the API call.
+var imageObj = {
+  smallImageUrl: 'https://imgs.xkcd.com/comics/standards.png',
+  largeImageUrl: 'https://imgs.xkcd.com/comics/standards.png'
+};
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+
+    // alexa.appId = 'amzn1.echo-sdk-ams.app.1234';
+    // alexa.dynamoDBTableName = 'YourTableName'; // creates new table for session.attributes
+
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('MyIntent');
+    },
+    'Unhandled': function () {
+        this.emit(':ask', 'Sorry I did not get that.', 'Who do you want to know about?');
+    },
+    'MyIntent': function () {
+        // Hero slot for the name of the comic book character in question.
+        var heroSlot = this.event.request.intent.slots.Hero;
+        var heroName;
+        if (heroSlot && heroSlot.value) {
+          heroName = heroSlot.value.toLowerCase();
         }
-        */
 
-        if (event.session.new) {
-            onSessionStarted({requestId: event.request.requestId}, event.session);
-        }
+        httpsGet(heroName,  (myResult) => {
+          console.log("sent     : " + heroName);
+          console.log("received : " + myResult);
 
-        if (event.request.type === "LaunchRequest") {
-            onLaunch(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === "IntentRequest") {
-            onIntent(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === "SessionEndedRequest") {
-            onSessionEnded(event.request, event.session);
-            context.succeed();
-        }
-    } catch (e) {
-        context.fail("Exception: " + e);
+          // SSML has trouble with ampersands.  Replacing them with the word "and".
+          myResult = myResult.replace("&", "and");
+
+          // Set the Title and Content for the card in the Alexa App.
+          var cardTitle = heroName;
+          var cardContent = myResult;
+
+          // Send the response to Alexa
+          this.emit(':tellWithCard', myResult, cardTitle, cardContent, imageObj);
+        });
     }
 };
 
-/**
- * Called when the session starts.
- */
-function onSessionStarted(sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId +
-        ", sessionId=" + session.sessionId);
-}
 
-/**
- * Called when the user launches the skill without specifying what they want.
- */
-function onLaunch(launchRequest, session, callback) {
-    console.log("onLaunch requestId=" + launchRequest.requestId +
-        ", sessionId=" + session.sessionId);
+//    END of Intent Handlers {} ========================================================================================
+// 3. Helper Function  =================================================================================================
 
-    // Dispatch to your skill's launch.
-    getWelcomeResponse(callback);
-}
 
-/**
- * Called when the user specifies an intent for this skill.
- */
-function onIntent(intentRequest, session, callback) {
-    console.log("onIntent requestId=" + intentRequest.requestId +
-        ", sessionId=" + session.sessionId);
+var https = require('https');
+// https is a default part of Node.JS.  Read the developer doc:  https://nodejs.org/api/https.html
+// try other APIs such as the current bitcoin price : https://btc-e.com/api/2/btc_usd/ticker  returns ticker.last
 
-    var intent = intentRequest.intent,
-        intentName = intentRequest.intent.name;
+function httpsGet(hero, callback) {
 
-    // Dispatch to your skill's intent handlers
-    if ("getHeroIntent" === intentName) {
-        getHero(intent, session, callback);  
-    } else if ("AMAZON.HelpIntent" === intentName) {
-        getWelcomeResponse(callback);
-    } else {
-        throw "Invalid intent";
-    }
-}
+    // GET is a web service request that is fully defined by a URL string
+    // Try GET in your browser:
+    // https://cp6gckjt97.execute-api.us-east-1.amazonaws.com/prod/stateresource?usstate=New%20Jersey
 
-/**
- * Called when the user ends the session.
- * Is not called when the skill returns shouldEndSession=true.
- */
-function onSessionEnded(sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId +
-        ", sessionId=" + session.sessionId);
-    // Add cleanup logic here
-}
-
-// --------------- Functions that control the skill's behavior -----------------------
-
-function getWelcomeResponse(callback) {
-    // If we wanted to initialize the session to have some attributes we could add those here.
-    var sessionAttributes = {};
-    var cardTitle = "Welcome";
-    var speechOutput = "Welcome to the Marvel API Alexa app;" +
-        "Ask me about your favorite marvel character";
-    // If the user either does not reply to the welcome message or says something that is not
-    // understood, they will be prompted again with this text.
-    var repromptText = "Hmmm, Ask me about your favorite marvel character";
-    var shouldEndSession = false;
-
-    callback(sessionAttributes,
-        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-}
-
-function getHero(intent, session, callback) {
-  var cardTitle = intent.name;
-  var repromptText = "";
-  var sessionAttributes = {};
-  var shouldEndSession = true;
-  var speechOutput = "";
-  
-  var hero;
-  
-  console.log("HERO >>>");
-  console.log(intent.slots.Hero.value);
-  console.log("<<< HERO");
-  
-  if(intent.slots.Hero.value){
-    hero = intent.slots.Hero.value;      
-  } else {
-    // TODO: If no hero in the slot, then reask user
-    speechOutput = "I don't know who you are asking about"
-  }
-  
-  getHeroFromMarvel(hero, function(heroes){
-      
-    if(heroes.data.count > 0) {
-        //speechOutput = "I have found " + heroes.data.count + " hero by that name;";
-        //TODO: I know this is kind of ghetto, but I just assume first character returned was the one asked for.
-        var hero = heroes.data.results[0];
-        if(hero.description === '') {
-            speechOutput = speechOutput + "Marvel doesn't seem to have a description for ";
-            speechOutput = speechOutput + hero.name + ", but lists that ";
-            speechOutput = speechOutput + hero.name + " was in the following comics; ";
-            speechOutput = speechOutput + hero.comics.items[0].name + "; ";
-            speechOutput = speechOutput + hero.comics.items[1].name + ", and, ";
-            speechOutput = speechOutput + hero.comics.items[2].name;
-        } else {
-            speechOutput = hero.name + ";";
-            speechOutput = speechOutput + hero.description;
-        }
-
-    } else {
-        speechOutput = "Marvel could not find the character you asked about.";
-    }
-    
-    callback(sessionAttributes,
-      buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-  });
-
-}
-
-// --------------- Helpers that build all of the responses -----------------------
-
-function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
+    // Update these options with the details of the web service you would like to call
+    var options = {
+        host: 'comicvine.gamespot.com',
+        port: 443,
+        path: '/api/search/?api_key=' + comicvine_apikey + '&field_list=count_of_issue_appearances,deck,image&resources=character&format=json&query=' + encodeURIComponent(hero),
+        method: 'GET',
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
         },
-        card: {
-            type: "Simple",
-            title: "SessionSpeechlet - " + title,
-            content: "SessionSpeechlet - " + output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
+
+        // if x509 certs are required:
+        // key: fs.readFileSync('certs/my-key.pem'),
+        // cert: fs.readFileSync('certs/my-cert.pem')
+    };
+
+    var req = https.request(options, res => {
+        res.setEncoding('utf8');
+        var returnData = "";
+
+        res.on('data', chunk => {
+            returnData = returnData + chunk;
+        });
+
+        res.on('end', () => {
+            // We have now received the raw return data in the returnData variable.
+            // We can see it in the log output via:
+            // console.log(JSON.stringify(returnData))
+            // We may need to parse through it to extract the needed data
+
+            var results = JSON.parse(returnData).results;
+
+            var pop = 'Oops.  Something went wrong.';
+
+            // Check if got any results from the API.
+            if (results.length > 0) {
+
+              // Sort search results by count of appearances.  Assume that the highest count is the most accurate result.
+              // TODO: Change this to loop through the array and get the highest count instead of sorting.
+              results.sort(function(a, b) {
+                return parseInt(b.count_of_issue_appearances) - parseInt(a.count_of_issue_appearances);
+              });
+
+              // Get the deck value of this character.  This is a short description of the character.
+              // TODO: Validate that deck is not null.
+              pop = results[0].deck;
+
+              imageObj.smallImageUrl = results[0].image.super_url;
+              imageObj.largeImageUrl = results[0].image.super_url;
+
+            } else {
+              pop = 'I did not find the Hero or Villain you were looking for.';
             }
-        },
-        shouldEndSession: shouldEndSession
-    };
+
+            callback(pop);  // this will execute whatever function the caller defined, with one argument
+        });
+    });
+    req.end();
 }
 
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: "1.0",
-        sessionAttributes: sessionAttributes,
-        response: speechletResponse
-    };
-}
-
-function getHeroFromMarvel(hero, callback) {
-    var PRIV_KEY = "<marvel private key>";
-    var API_KEY = "<marvel api key>";
-    var ts = new Date().getTime();
-    var hash = crypto.createHash('md5').update(ts + PRIV_KEY + API_KEY).digest('hex');
-    var pathurl = '/v1/public/characters?name=' + encodeURIComponent(hero) + "&apikey=" + API_KEY;
-    pathurl += "&ts="+ts+"&hash="+hash;
-    console.log(pathurl);
-  return http.get({
-    host: 'gateway.marvel.com',
-    path: pathurl
-  }, function(response) {
-    // Continuously update stream with data
-    var body = '';
-    response.on('data', function(d) {
-      body += d;
-    });
-    response.on('end', function() {
-        console.log(body);
-      callback(JSON.parse(body));
-    });
-  });
-}
